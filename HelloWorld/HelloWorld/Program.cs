6b102f1df5d130ee28d2a324aa82c1a2fc18c6f2
Sieffre <email@finnerty.id.au>
2015-11-05 15:57:04 +1100

    Branched then changed source

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("Hello - Line2");
             Console.ReadKey();
         }
     }
